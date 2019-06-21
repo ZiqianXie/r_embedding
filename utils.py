26a525fa7ec1d414a808e0455fcbcd8de5bece95
ZiqianXie <z.xie4@umiami.edu>
2019-06-21 16:11:25 -0500

    fix dataset len bug

--- utils.py
+++ utils.py
@@ -53,7 +53,7 @@ class MyDataset(Dataset):
                 self.files.append(f)
 
     def __len__(self):
-        return len(self.data)
+        return len(self.files)
 
     def __getitem__(self, idx):
         img = cv2.imread(self.dataset_path.strip('/') + '/' + self.files[idx])
